17dd095e9bc87d4077dfd6284b0108178902116e
amerriman <amm434@gmail.com>
2015-10-04 17:43:32 -0600

    bugs

--- server/_config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.mongoURI = {
-  development: "mongodb://localhost/node-setofcards'",
-  production: "mongodb://heroku_3kmn6hks:6f59lavhkcntktlhlc6dh7l7be@ds035653.mongolab.com:35653/heroku_3kmn6hks"
-};
-
-module.exports = config;
